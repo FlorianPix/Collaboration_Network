348c4c7e9022569ee59980a8217bae10faad4150
Oliver Portee <oliver.portee@gmail.com>
2021-11-04 08:59:38 +0100

    put paper saving code in separate function

--- data_collection/data_collection.py
+++ data_collection/data_collection.py
@@ -3,7 +3,7 @@ import pickle
 from Bio import Entrez, Medline
 
 
-def get_papers(my_query, max_papers, my_email="florian.pix@mailbox.tu-dresden.de"):
+def get_papers(my_query, max_papers, my_email="oliver.portee@mailbox.tu-dresden.de"):
     # Get articles from PubMed
     Entrez.email = my_email
     record = Entrez.read(Entrez.esearch(db="pubmed", term=my_query, retmax=max_papers))
@@ -14,10 +14,11 @@ def get_papers(my_query, max_papers, my_email="florian.pix@mailbox.tu-dresden.de
     # Converting it to a list, makes it permanently accessible.
     return list(records)
 
+def save_papers(filename, max_number, search_term="[AD]"):
+    papers = get_papers(search_term, max_number)
+    with open(f'saved_papers/{filename}', 'wb') as outp:
+        pickle.dump(papers, outp, pickle.HIGHEST_PROTOCOL)
+        print(f'saved papers in saved_papers/{filename}')
 
-my_query = "clustering[ti] algorithm"  # query in title and abstract
-max_papers = 10  # limit the number of
-papers = get_papers(my_query, max_papers)
-
-with open('papers.pkl', 'wb') as outp:
-    pickle.dump(papers, outp, pickle.HIGHEST_PROTOCOL)
+if __name__ == '__main__':
+    save_papers('papers.pkl', 100000)
