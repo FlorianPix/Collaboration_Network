bb789c0cb278647975eac47bb42d57522aa9d3e1
Oliver Portee <oliver.portee@gmail.com>
2021-11-06 10:53:12 +0100

    put file reading and writing into io.py file

---
+++ io.py
@@ -0,0 +1,25 @@
+
+import pickle
+from typing import Any
+
+from processing.model import Coordinates, Location
+
+def save_papers_pickle(papers: list[dict[str, Any]], filename: str):
+    """save papers (dict format) to file in folder data/papers"""
+    with open(f"data/papers/{filename}", 'wb') as papers_file:
+        pickle.dump(papers, papers_file, pickle.HIGHEST_PROTOCOL)
+
+def get_papers_pickle(filename: str) -> dict[str, Any]:
+    """retrive papers (dict format) from file in folder data/papers"""
+    with open(f"data/papers/{filename}", 'rb') as papers_file:
+        return pickle.load(papers_file)
+
+def save_coords_pickle(coords: dict[Location, Coordinates], filename: str):
+    """save coordinates (dict with locations as keys) to file in folder data/coordinates"""
+    with open(f"data/coordinates/{filename}", 'wb') as coords_file:
+        pickle.dump(coords, coords_file, pickle.HIGHEST_PROTOCOL)
+
+def get_coords_pickle(filename: str) -> dict[Location, Coordinates]:
+    """retrieve coordinates (dict with locations as keys) from file in folder data/coordinates"""
+    with open(f"data/coordinates/{filename}", 'rb') as coords_file:
+        return pickle.load(coords_file)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-from processing.extraction import get_location_naive, get_papers_with_locations
-from processing.paper_reading import get_papers_pickle
-from processing.coordinates import get_location_coordinates
 
+import time
+from processing.extraction import get_location_naive
 
 def test_fail_rate(affiliation_list: list[str]):
     """Print fail rate"""
@@ -14,14 +12,5 @@ def test_fail_rate(affiliation_list: list[str]):
         if get_location_naive(affil) is None:
             fail_counter += 1
     print(time.time() - time1)
-    print(f"number of affiliations: {len(affiliation_list)}, fails: {fail_counter}, ratio: {fail_counter / len(affiliation_list)}")
-
-
-papers = get_papers_pickle("papers010000.pkl")
-print(f"number of papers: {len(papers)}")
-
-papers = get_papers_with_locations(papers)
-time1 = time.time()
-coords = get_location_coordinates(papers)
-with open("data/")
-print(time.time() - time1)
+    print(f"number of affiliations: {len(affiliation_list)}, "\
+        f"fails: {fail_counter}, ratio: {fail_counter / len(affiliation_list)}")
