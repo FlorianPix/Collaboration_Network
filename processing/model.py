eed24572e0b455eb508f0fdbb173b975c7418130
Oliver Portee <oliver.portee@gmail.com>
2021-11-06 01:56:01 +0100

    use @dataclass for model classes

--- processing/model.py
+++ processing/model.py
@@ -1,32 +1,27 @@
 
 from typing import Optional
+from dataclasses import dataclass
 
 
+@dataclass(frozen=True, unsafe_hash=True)
 class Location:
     """Class containing a city, a country and optionally a state"""
-
-    def __init__(self, city: str, region: Optional[str], country: str):
-        self.city: str = city
-        self.state: Optional[str] = region
-        self.country: str = country
-
-    def __repr__(self) -> str:
-        return f"Location({self.city}, {self.state}, {self.country})"
+    city: str
+    state: Optional[str]
+    country: str
 
 
+@dataclass(frozen=True, unsafe_hash=True)
 class Coordinates:
     """Coordinates represented by latitude and longitude (float values)"""
 
-    def __init__(self, lat: float, long: float):
-        self.lat: float = lat
-        self.long: float = long
-
-    def __repr__(self) -> str:
-        return f"Coords({self.lat}, {self.long})"
+    lat: float
+    long: float
 
 
+@dataclass(frozen=True, unsafe_hash=True)
 class Paper:
     """Class representing paper"""
 
-    def __init__(self, locations: list[Location]):
-        self.locations: list[Location] = locations
+    pmid: int
+    locations: list[Location]
