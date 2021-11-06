0008462be5c74908aba59d9c02cfcd5ac76e8cc8
Oliver Portee <oliver.portee@gmail.com>
2021-11-06 17:31:51 +0100

    reduce number of papers to 500

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import processing.network as n
 
 # run data_collection.py script once to get the files
 
-papers = get_papers_with_locations(io.get_papers_pickle("papers10000.pkl"))
-coords = io.get_coords_pickle("coordinates10000.pkl")
+papers = get_papers_with_locations(io.get_papers_pickle("papers500.pkl"))
+coords = io.get_coords_pickle("coordinates500.pkl")
 
 # build a graph with locations (containing a city and country, respectively);
 # this graph will have the number of co-occurrences in papers as edge weights
