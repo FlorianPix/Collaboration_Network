f12efff8f45d6427123a6bc18512edb3fa565a63
Oliver Portee <oliver.portee@gmail.com>
2021-11-18 23:02:02 +0100

    extraction handling us state instead of country

--- processing/extraction.py
+++ processing/extraction.py
@@ -1,29 +1,38 @@
 """Extract location data out of string"""
-from typing import Any, Optional
-import time
+from typing import Any, Optional, Tuple
 import json
 import geograpy
 import geotext
 
 from .model import Location, Paper
-from .util import progressbar
 
 __countries = json.load(open("data/dictionaries/countries.json", 'rb'))
 
-def __extract_country(affiliation: list[str]) -> Optional[str]:
-    target = affiliation[-1].strip()
+def __extract_state(target: str) -> Optional[Tuple[str, str]]: # (state, country)
+    for country, data in __countries.items():
+        states = data.get("states", [])
+        for state in states:
+            if state.startswith(target) or target.startswith(state):
+                return (state, country)
+    return None
+
+
+def __extract_country(target: str) -> Optional[Tuple[Optional[str], str]]: # (state, country)
     for country in __countries:
         if country.startswith(target) or target.startswith(country):
-            return country
+            return None, country
     for country, data in __countries.items():
         for alt in data["alt"]:
             if alt.startswith(target) or target.startswith(alt):
-                return country
+                return None, country
     for country, data in __countries.items():
         code = data["code"]
         if target.startswith(code) or target.endswith(code):
-            return country
-    ## print(f"Unknown country: '{target}'")
+            return None, country
+    result = __extract_state(target)
+    if result is not None:
+        return result
+    # print(f"Unknown country: '{target}'")
     return None
 
 # there's a lot of room for optimization here
@@ -33,14 +42,18 @@ def get_location_naive(text: str) -> Optional[Location]:
         text = text[:-2]
     parts = text.split(',')
     if len(parts) < 3:
-        ## print("Affiliation too short:", text)
+        parts = text.split(' ')
+    if len(parts) < 3:
+        # print(f"Bad format: {text}")
         return None
-    country = __extract_country(parts)
-    if country is None:
+    target = parts[-1].strip().lower()
+    result = __extract_country(target)
+    if result is None:
         return None
-    # TODO: special handling for USA states
+    state, country = result
     city = parts[-2].strip()
-    return Location(city, None, country)
+    print(city)
+    return Location(city, state, country)
 
 # this is extremely slow and does not work very well either;
 # need to run `geograpy-nltk` in order for it to work;
@@ -66,25 +79,20 @@ def get_location_geotext(text: str) -> Optional[Location]:
 def get_papers_with_locations(papers: list[dict[str, Any]]) -> list[Paper]:
     """return list of papers containing list of locations"""
     result: list[Paper] = []
-    for paper in progressbar(papers, "extracting locations: "):
+    fail_count = 0
+    affiliation_count = 0
+    # for paper in progressbar(papers, "extracting locations: "):
+    for paper in papers:
         if 'AD' in paper and 'PMID' in paper:
             locations = []
             for affiliation in paper['AD']:
+                affiliation_count += 1
                 location = get_location_naive(affiliation)
                 if location is not None:
                     locations.append(location)
+                else:
+                    fail_count += 1
             result.append(Paper(int(paper['PMID']), locations))
+    if affiliation_count > 0:
+        print(f"Extraction fail rate: {round(fail_count / affiliation_count * 100, 2)}% (failed: {fail_count}, total: {affiliation_count})")
     return result
-
-def test_fail_rate(affiliation_list: list[str]):
-    """Print fail rate"""
-    fail_counter = 0
-    time1 = time.time()
-    for affil in affiliation_list:
-        # if get_location_geograpy(affiliation) is None:
-        # if get_location_geotext(affiliation) is None:
-        if get_location_naive(affil) is None:
-            fail_counter += 1
-    print(time.time() - time1)
-    print(f"number of affiliations: {len(affiliation_list)}, "\
-        f"fails: {fail_counter}, ratio: {fail_counter / len(affiliation_list)}")
