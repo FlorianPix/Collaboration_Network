a713916e7fe85019f37ee3c44d0ec0dd64cb2435
albrechtl982 <johannalbrecht.lange@t-online.de>
2021-11-25 12:26:42 +0100

    removed duplicates

--- processing/network.py
+++ processing/network.py
@@ -13,7 +13,8 @@ def build_city_graph(papers: list[Paper], coords: dict[Location, Optional[Coordi
     """
     graph = nx.Graph()
     for paper in progressbar(papers, "building graph: "):
-        valid_locations = filter(lambda l: l in coords and coords[l] is not None, paper.locations)
+        locations = list(set(paper.locations))
+        valid_locations = filter(lambda l: l in coords and coords[l] is not None, locations)
         for (l_1, l_2) in itertools.combinations(valid_locations, 2):
             if graph.has_edge(l_1, l_2):
                 graph[l_1][l_2]['weight'] += 1
