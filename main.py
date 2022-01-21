e595b85941f584c49c0f3577fd0ad608f40168bb
Karl Lautenschlaeger <lautenschlaeger.karl@gmx.de>
2022-01-21 08:53:24 +0100

    data folder added

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from processing.plotter import Plotter
 import plotly.graph_objects as go
 import numpy as np
 
-
+paperCount = 200000
 topic = "Alzheimer"
-searcher = Searcher(papers="./data/papers/papers.pkl", countryJson="./data/dictionaries/countries.json", topic=topic)
+searcher = Searcher(papers="/home/karlo/Desktop/UNI/9. Semester/AI/IntelligenteSysteme/IntelligenteSystemeProject/Collaboration_Network/data/papers/papers.pkl", countryJson="/home/karlo/Desktop/UNI/9. Semester/AI/IntelligenteSysteme/IntelligenteSystemeProject/Collaboration_Network/data/dictionaries/countries.json", topic=topic)
 searcher.findTotalResearchPerCountry()
 searcher.findTopicInPapers()
-
-plotter = Plotter(countryPopulation="./data/dictionaries/countryPopulation.json")
-
-plotter.setData(data=searcher.result, topic=topic, paperCount=200000)
-
+plotter = Plotter(countryPopulation="/home/karlo/Desktop/UNI/9. Semester/AI/IntelligenteSysteme/IntelligenteSystemeProject/Collaboration_Network/data/dictionaries/countryPopulation.json")
+plotter.setData(data=searcher.result, topic=topic, paperCount=paperCount)
 plotter.plotResearchRatio()
 plotter.plotResearchPopulationRatio()
+
+# new topic:
+topic = "Tourette"
+searcher.setCountries()
+searcher.setTopic(topic=topic)
+searcher.findTotalResearchPerCountry()
+searcher.findTopicInPapers()
+plotter.setData(data=searcher.result, topic=topic, paperCount=paperCount)
+plotter.plotResearchRatio()
